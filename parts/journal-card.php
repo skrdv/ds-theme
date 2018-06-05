73d03dbdc3eea52c5891d59defbc6b7ae3365dc7
Dmitriy Skudarnov <skrdvx@gmail.com>
2018-06-05 14:25:46 +0300

    Update Journal listing layout and card for mobile
    Dmitriy

--- parts/journal-card.php
+++ parts/journal-card.php
@@ -1,16 +1,33 @@
-<div class="cardJournal">
+<div class="cardJournal hidden-xs">
 	<div class="cardJournal-body">
-		<a class="cardJournal-title is-<?php the_field('journal-color') ?>">
+		<h3 class="cardJournal-title is-<?php the_field('journal-color') ?>">
 			<?php the_title(); ?>
-		</a>
+		</h3>
     <div class="cardJournal-excerpt">
       <?php the_excerpt(); ?>
     </div>
+		<?php if(has_post_thumbnail()): ?>
 		<div class="cardJournal-image">
       <?php the_post_thumbnail(array(540, 320)); ?>
     </div>
 		<div class="cardJournal-link">
       <a class="btn btn-<?php the_field('journal-color') ?>" href="<?php the_permalink(); ?>">Go to journal</a>
     </div>
+		<?php endif; ?>
 	</div>
 </div>
+<a class="cardJournal visible-xs" href="<?php the_permalink(); ?>">
+	<div class="cardJournal-body">
+		<h3 class="cardJournal-title is-<?php the_field('journal-color') ?>">
+			<?php the_title(); ?>
+		</h3>
+    <div class="cardJournal-excerpt">
+      <?php the_excerpt(); ?>
+    </div>
+		<?php if(has_post_thumbnail()): ?>
+		<div class="cardJournal-image">
+      <?php the_post_thumbnail(array(540, 320)); ?>
+    </div>
+		<?php endif; ?>
+	</div>
+</a>
