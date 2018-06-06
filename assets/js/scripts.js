be0972b36878299d8e9488239ae8e93ad50f7a47
Dmitriy Skudarnov <skrdvx@gmail.com>
2018-06-06 15:09:23 +0300

    Update menu actions on home
    Dmitriy

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -12,7 +12,6 @@ function menuHomeTabs() {
       linkName = $(this).attr('class');
       tabClass = '.tab-link.'+linkName;
       paneClass = '.tab-pane.'+linkName;
-      console.log(linkName);
 
       $('.startTabs-item a').removeClass('active');
       $(tabClass).addClass('active');
@@ -20,18 +19,19 @@ function menuHomeTabs() {
       $('.tab-content>div').removeClass('active');
       $(paneClass).addClass('active');
 
+      $('#navbarHeader').removeClass('show');
+      $('.menu-toggler.navbar-toggler').removeClass('collapsed');
+
+      $('html, body').animate({
+        scrollTop: 600
+      }, 1000);
     });
 
   }
 
 }
 
-
-$(document).ready(function() {
-
-  menuHomeTabs();
-
-
+function slickSlider() {
 
   $('.slickSlider').slick({
     infinite: false,
@@ -48,52 +48,13 @@ $(document).ready(function() {
     console.log(slideno);
   });
 
+}
 
 
-  // $('a[data-slide]').click(function(e) {
-  //   var slideno = $(this).data('slide');
-  //   $('.slickSlider').slick('slickGoTo', slideno);
-  // });
+$(document).ready(function() {
 
-  // if($('.slick-arrow').length){
-  // 	$('.slick-track').addClass('normal-width');
-  //   $('.slick-slide').addClass('normal-width');
-  // } else {
-  //   $('.slick-track').addClass('full-width');
-  //   $('.slick-slide').addClass('full-width');
-  // }
-
-  // // Custom sticky
-  // var stickyToggle = function(sticky, stickyWrapper, scrollElement) {
-  //   var stickyHeight = sticky.outerHeight();
-  //   var stickyTop = stickyWrapper.offset().top;
-  //   if (scrollElement.scrollTop() >= stickyTop){
-  //     stickyWrapper.height(stickyHeight);
-  //     sticky.addClass("is-sticky");
-  //   }
-  //   else{
-  //     sticky.removeClass("is-sticky");
-  //     stickyWrapper.height('auto');
-  //   }
-  // };
-  //
-  // // Find all data-toggle="sticky-onscroll" elements
-  // $('[data-toggle="sticky-onscroll"]').each(function() {
-  //   var sticky = $(this);
-  //   var stickyWrapper = $('<div>').addClass('sticky-wrapper'); // insert hidden element to maintain actual top offset on page
-  //   sticky.before(stickyWrapper);
-  //   sticky.addClass('sticky');
-  //
-  //   // Scroll & resize events
-  //   $(window).on('scroll.sticky-onscroll resize.sticky-onscroll', function() {
-  //     stickyToggle(sticky, stickyWrapper, $(this));
-  //   });
-  //
-  //   // On page load
-  //   stickyToggle(sticky, stickyWrapper, $(window));
-  // });
-
-
+  menuHomeTabs();
+  slickSlider();
 
 });
 
